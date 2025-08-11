40e5e286afb483953b17ee3199ec008ee41469f3
Roman Eriksen <roman.eriksen@no.experis.com>
2025-08-11 12:53:15 +0200

    Add bagel test

---
+++ exercise.main/IProduct.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise.main
+{
+    public interface IProduct
+    {
+        double Price { get; set;  } 
+    }
+}
--- exercise.tests/UnitTest1.cs
+++ exercise.tests/UnitTest1.cs
@@ -2,14 +2,19 @@ namespace exercise.tests;
 
 public class Tests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
+    //[SetUp]
+    //public void Setup()
+    //{
+    //}
 
     [Test]
-    public void Test1()
+    public void TestAddBagelToBasket()
     {
+        Bagel bagel = new Bagel();
+        Basket basket = new Basket();
+        Customer customer = new Customer();
+        customer.AddProduct(bagel);
+        Assert.That(basket.Count > 1);
         Assert.Pass();
     }
 }
